3576d64093da3e7ef155974b913f064d479715a9
Lars Brix <laxisbrix@gmail.com>
2018-03-20 19:46:27 +0100

    .

--- scripts/checkLeaderboardIntegrity/checkBomb.js
+++ scripts/checkLeaderboardIntegrity/checkBomb.js
@@ -1,19 +1,18 @@
 const _ = require("lodash");
 
-const blocks = ["▁","▂","▃","▅","▆","▇","█"];
-const getBlock = (perc) => {
+const blocks = ["▁", "▂", "▃", "▅", "▆", "▇", "█"];
+const getBlock = perc => {
     const maxChars = blocks.length - 1;
     const charIndex = Math.floor(perc * maxChars);
     return blocks[charIndex];
 };
 
-module.exports = function (player) {
+module.exports = function(player) {
     const progressions = player.progressions.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-
     /** grab all deltas for the known playdays */
-    const played = progressions.slice(1)
-        .reduce((acc, prev, i) => {
+    const played = progressions.slice(1).reduce(
+        (acc, prev, i) => {
             const curr = progressions[i];
 
             const pBomb = _.get(prev, "stats.bomb.played", 0);
@@ -36,24 +35,24 @@ module.exports = function (player) {
             const cCasual = _.get(curr, "stats.casual.played", 0);
             const dCasual = cCasual - pCasual;
 
-
             /** get the amount of bomb games in ranked queue. be optimistic */
             const bombInRanked = Math.min(dRanked, dBomb);
             const char = getBlock(bombInRanked / (dRanked || 1));
-            return (dRanked > 0)
+            return dRanked > 0
                 ? {
-                    bomb: acc.bomb + dBomb,
-                    secure: acc.secure + dSecure,
-                    hostage: acc.hostage + dHostage,
-                    total: acc.total + dBomb + dSecure + dHostage,
-                    ranked: acc.ranked + dRanked,
-                    casual: acc.casual + dCasual,
-                    bombInRanked: acc.bombInRanked + bombInRanked,
-                    trend: acc.trend + char,
-                    datapoints: acc.datapoints + 1
-                }
+                      bomb: acc.bomb + dBomb,
+                      secure: acc.secure + dSecure,
+                      hostage: acc.hostage + dHostage,
+                      total: acc.total + dBomb + dSecure + dHostage,
+                      ranked: acc.ranked + dRanked,
+                      casual: acc.casual + dCasual,
+                      bombInRanked: acc.bombInRanked + bombInRanked,
+                      trend: acc.trend + char,
+                      datapoints: acc.datapoints + 1,
+                  }
                 : acc;
-        }, {
+        },
+        {
             bomb: 0,
             secure: 0,
             hostage: 0,
@@ -63,7 +62,8 @@ module.exports = function (player) {
             bombInRanked: 0,
             datapoints: 0,
             trend: "",
-        });
+        },
+    );
 
     played.start = progressions[progressions.length - 1].created_at;
 
@@ -76,15 +76,13 @@ module.exports = function (player) {
      * this is heavily favored towards the player! the mode spread is way more uniform in practice.
      * we will however always calculate in favor of the player to reduce false positives.
      */
-    const bombP = played.total > 0
-        ? played.bombInRanked / (played.ranked || Infinity)
-        : 0;
+    const bombP = played.total > 0 ? played.bombInRanked / (played.ranked || Infinity) : 0;
 
     /** return our data to be displayed */
-    return{
+    return {
         platform: _.get(player, "platform"),
-        placement: _.get(player, "placements.global", null) +1,
-        // id: _.get(player, "id"),
+        placement: _.get(player, "placements.global", null),
+        id: _.get(player, "id"),
         name: _.get(player, "name"),
         bombPercentage: Number.parseFloat((bombP * 100).toFixed(2)),
         bomb: played.bomb,
@@ -95,12 +93,12 @@ module.exports = function (player) {
         rankedDays: played.datapoints,
         start: formatYear(played.start),
     };
-}
+};
 
 function average(arr) {
     return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 function formatYear(date) {
     const d = new Date(date);
-    return `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 }
--- scripts/checkLeaderboardIntegrity/index.js
+++ scripts/checkLeaderboardIntegrity/index.js
@@ -3,13 +3,11 @@ const checkBomb = require("./checkBomb");
 const _ = require("lodash");
 require("console.table");
 
-
-if(process.argv[2]) {
-    const res = api.getPlayer(process.argv[2])
-    .then(res => {
-        const result = res.map(checkBomb)
+if (process.argv[2]) {
+    const res = api.getPlayer(process.argv[2]).then(res => {
+        const result = res.map(checkBomb);
         console.table(result);
-    })
+    });
 } else {
     const pcProm = api.getLeaderboard("PC");
     const ps4Prom = api.getLeaderboard("PS4");
@@ -23,10 +21,9 @@ if(process.argv[2]) {
                 .filter(x => x.ranked >= 20)
                 .filter(x => x.rankedDays >= 3)
                 .filter(x => x.bombPercentage < 15)
-                // .sortBy(['bombPercentage'])
-                .sortBy(['platform', 'bombPercentage'])
-                .value()
-            console.log('%s players found', result.length);
+                .sortBy(["platform", "bombPercentage"])
+                .value();
+            console.log("%s players found", result.length);
             console.table(result);
         })
         .catch(console.error);
